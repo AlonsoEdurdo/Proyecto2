b7600c842c6677e6e23387dcf6c9d6c57bc4c81a
AlonsoEdurdo <eduardomoon89@gmail.com>
2025-12-02 12:42:36 -0600

    Add files via upload

---
+++ Actividad1U2/Actividad1U2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Actividad1U2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Universidad Abierta y a Distancia de México");
+            Console.WriteLine("División de Ciencias Exactas, Ingeniería y Tecnología");
+            Console.WriteLine("Programación .NET I");
+            Console.WriteLine("Alonso Eduardo Gómez Díaz");
+            Console.WriteLine("Matricula: ES1921009082");
+            Console.WriteLine("Actividad 1. Unidad 2");
+        }
+    }
+}
